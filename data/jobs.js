9471aca63d94afbafa001e43ccd3e04d2f19ef02
Walter Maniás <walter.manias@devgurus.io>
2021-04-13 12:28:16 -0300

    Seed script

---
+++ data/jobs.js
@@ -0,0 +1,9 @@
+module.exports = [
+  {
+    _id: "6075a226ff161500123f5b4b",
+    status: "COMPLETED",
+    leagueCode: "PL",
+    createdAt: "2021-04-13T13:52:38.325Z",
+    updatedAt: "2021-04-13T13:55:59.460Z",
+  },
+];
--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,9 @@ const mongoose = require("mongoose");
 const connect = async () => {
   try {
     const { databaseUri, databaseOptions } = config.get("settings");
-    await mongoose.connect(databaseUri, databaseOptions);
+    const connection = await mongoose.connect(databaseUri, databaseOptions);
     console.log("Database connected successfully...");
+    return connection;
   } catch (err) {
     console.error("Error while trying to connect to database...", err.message);
     process.exit(1);
